feb2b72f400700dbeada059e85bc076ec08531ce
CrimsonCrow <26500570+CrimsonCrow@users.noreply.github.com>
2021-02-22 02:03:34 +0000

    stack: test push and pop

--- src/main.c
+++ src/main.c
@@ -10,19 +10,9 @@ int main(void) {
         stack_push(stk, (void*)i);
     }
 
-    printf("printing stack\n");
-    for(size_t i = 0; i < stk->length; ++i) {
-        printf("%zu\n", i);
-    }
-
     printf("\npoping\n");
     stack_pop(stk);
     stack_pop(stk);
-
-    printf("\nprinting stack after pop\n");
-    for(size_t i = 0; i < stk->length; ++i) {
-        printf("%zu\n", i);
-    }
 
     stack_destroy(stk);
 
